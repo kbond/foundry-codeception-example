c9c9138cc1b58aacd0d64f64af3edc6be7711d91
Kevin Bond <kevinbond@gmail.com>
2020-10-14 10:48:30 -0400

    add codeception

---
+++ tests/functional/FirstCest.php
@@ -0,0 +1,14 @@
+<?php namespace App\Tests;
+use App\Tests\FunctionalTester;
+
+class FirstCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
